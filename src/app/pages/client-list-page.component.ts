78e66fb011848967e49f5171b1fa290b758ecf30
Elo Souza <elosouz.dev@gmail.com>
2024-05-02 11:02:17 -0400

    rotas

---
+++ src/app/pages/client-list-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-client-list',
+    templateUrl: './client-list-page.component.html'
+
+})
+
+export class ClientListPage {
+
+}
