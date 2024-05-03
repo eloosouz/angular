35cc6a41f29fe183bdcf4d4f34d33f8d6a5882d0
Elo Souza <elosouz.dev@gmail.com>
2024-05-03 15:19:57 -0400

    utilizando style binding para aplicar estilos

--- src/app/pages/binding-page-component.ts
+++ src/app/pages/binding-page-component.ts
@@ -6,8 +6,8 @@ import { Component } from "@angular/core";
     selector: "app-binding",
     template: `
         <h1>Imagem do Angular</h1>
-        <img [src]="imagem" [style]="{width: width}"/>
-        <div class="alert" [class]="{sucess: sucesso}"> 
+        <img [src]="imagem" [style]="{width: width, height: height}" />
+        <div class="alert" [class]="sucesso" [style.background-color]="backgroundcolor"> 
             Alerta
 
         </div>
@@ -28,12 +28,14 @@ import { Component } from "@angular/core";
     }
 )
 export class BindingPageComponent {
-    width = "300px"
-    imagem = "https://imgs.search.brave.com/dypTLVYvvTZUZ89czrAaAgmwEs-uIyyoaIyOF0_moco/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9j/L2NmL0FuZ3VsYXJf/ZnVsbF9jb2xvcl9s/b2dvLnN2Zw.svg"
-    
-    sucesso = true
+    width = "150px"
+    height= "200px"
+    imagem = "https://imgs.search.brave.com/dypTLVYvvTZUZ89czrAaAgmwEs-uIyyoaIyOF0_moco/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9j/L2NmL0FuZ3VsYXJf/ZnVsbF9jb2xvcl9s/b2dvLnN2Zw.svg" 
+    backgroundcolor= "blue" 
+    sucesso = "outra classe"
 
     enviarDados(){
-        this.sucesso = !this.sucesso
+       //  this.sucesso = !this.sucesso
+       this.backgroundcolor = "red"
     }
 }
