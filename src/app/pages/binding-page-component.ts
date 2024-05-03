917e32824781d7bc07f3ebbd3e6875542f3073da
Elo Souza <elosouz.dev@gmail.com>
2024-05-03 14:01:42 -0400

    utilizando class binding para aplicar estilos

--- src/app/pages/binding-page-component.ts
+++ src/app/pages/binding-page-component.ts
@@ -1,15 +1,39 @@
 
 import { Component } from "@angular/core";
 
-@Component ({
+@Component(
+    {
     selector: "app-binding",
     template: `
-    <h1>Imagem do Angular</h1>
-    <img [src]="imagem" [style]="{width: width}">
-    `
-    
-})
+        <h1>Imagem do Angular</h1>
+        <img [src]="imagem" [style]="{width: width}"/>
+        <div class="alert" [class]="{sucess: sucesso}"> 
+            Alerta
+
+        </div>
+        <button (click)="enviarDados()">Enviar dados</button>
+         `,
+         styles: [
+            `.alert {
+                width:200px;
+                height:100px;
+                border: 1px solid blue
+
+            }
+            .sucess {
+                border: 5px solid green;
+            }
+            `
+        ]
+    }
+)
 export class BindingPageComponent {
     width = "300px"
     imagem = "https://imgs.search.brave.com/dypTLVYvvTZUZ89czrAaAgmwEs-uIyyoaIyOF0_moco/rs:fit:500:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy9j/L2NmL0FuZ3VsYXJf/ZnVsbF9jb2xvcl9s/b2dvLnN2Zw.svg"
+    
+    sucesso = true
+
+    enviarDados(){
+        this.sucesso = !this.sucesso
+    }
 }
